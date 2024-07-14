ab902629d22298ca28ac372b4b55389100b22315
Okosa Leonard <okosaleonel@gmail.com>
2024-07-14 19:37:53 +0100

    cc

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,12 @@ export default function Hero() {
 
   return (
     <div className='bg-[#0F0F0F] w-full h-[100vh]'>
-      <div className=" flex items-center relative justify-evenly flex-col">
+      <div className=" flex items-center relative justify-evenly">
        <div className=" md:w-1/3 w-50vh">
         <Lottie animationData={kimetsu}/>
         </div>
-        <div className=" z-10 md:absolute ">
-         <h1 className=" font-Sunage  md:text-[7rem] text-[3rem] md:p-0 p-4 text-[#fbfbfb]">Lenny The Dev</h1>
+        <div className=" z-10 absolute ">
+         <h1 className=" font-Sunage  md:text-[7rem] text-[1.5rem] text-[#fbfbfb]">Lenny The Dev</h1>
          </div>
         </div>
     </div>
